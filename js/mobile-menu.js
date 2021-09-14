d9acfac5d3c6ea18b6b72a39a8df8e83c8848964
Preisy <43937512+Preisy@users.noreply.github.com>
2021-09-14 22:09:16 +0300

    Add files via upload

---
+++ js/mobile-menu.js
@@ -0,0 +1,13 @@
+var elem = document.getElementsByClassName('navigation')[0]
+var img = document.querySelector('img[alt="mobile-menu"]');
+img.addEventListener('click', function() {
+  tent();
+  if(elem.style.transform == "translateX(100%)") {
+    elem.style.cssText = "transform: translateX(0);"
+    return true
+  } else if (elem.style.transform == "translateX(0px)") {
+    elem.style.cssText = "transform: translateX(100%);"
+    return true
+  }
+  elem.style.cssText = "transform: translateX(0);";
+})
